8256f3124f5da6564f1de40c2c280384a34c4c42
unknown <synergy2411@outlook.com>
2020-08-14 11:29:01 +0530

    validation added

--- 02-playground/src/NotesApp/NewNoteForm/NewNoteForm.js
+++ 02-playground/src/NotesApp/NewNoteForm/NewNoteForm.js
@@ -4,25 +4,41 @@ class NewNoteForm extends React.Component{
 
     state ={
         title : '',
-        body : ''
+        body : '',
+        errorMessage : '',
+        hasError : false
     }
 
     onAddNote = event => {
         event.preventDefault();
-        console.log("Title : ", this.state.title)
-        console.log("Body : ", this.state.body);
-        const {title, body} = this.state;
+        if(this.state.title === '' || this.state.body === ''){
+            this.setState({errorMessage : "Both fields are mandatory", hasError : true})
+        }else{
+            const {title, body} = this.state;
         this.props.onAddNewNote(title, body);
+        }
+        
+    }
+
+    onTitleChange = (event) => {
+        this.setState({
+            title : event.target.value
+        })
     }
     render() {
         const style = {
             margin : "10px",
             padding : "8px"
         }
+        let errorMsg = null;
+        if(this.state.hasError){
+            errorMsg = <p className="alert alert-danger">*{this.state.errorMessage}</p>
+        }
         return (
             <div className="col-8 offset-2">
                 <div className="card">
                 <form style={style}>
+                    {errorMsg}
                     <div className="row">
                         <div className="col-4">
                             <label>Title : </label>
@@ -31,7 +47,7 @@ class NewNoteForm extends React.Component{
                             <input type="text" 
                                 className="form-control" 
                                 name="title" 
-                                onChange = {event => this.setState({title : event.target.value})}
+                                onChange = {this.onTitleChange}
                                 value = {this.state.title}/>
                         </div>
                     </div>
